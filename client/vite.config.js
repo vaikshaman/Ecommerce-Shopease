bcdf57d537114db238a73d8ba82c5a87127295e2
vaikshaman <133702921+vaikshaman@users.noreply.github.com>
2024-06-25 23:46:30 +0530

    cc

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      "/api": "https://sparktech-ecommerce-youtube.onrender.com",
-      "/upload": "https://sparktech-ecommerce-youtube.onrender.com",
+      "/api": "",
+      "/upload": "",
     },
   },
 })
